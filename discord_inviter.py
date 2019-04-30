98569cd53cca268375a2e88c824d7b8f955f2895
Polyanin Mike <ya.polyanin-mike@yandex.ru>
2019-04-30 19:12:04 +0300

    It can make connection at least

---
+++ discord_inviter.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from configparser import ConfigParser
+
+config_file = 'set.ini'
+config = ConfigParser()
+config.read(config_file)
+BOT_TOKEN = config['Bot']['token']
+
+client = commands.Bot(command_prefix = '$')
+
+@client.event
+async def on_ready():
+    print('Ready\n ---- \n')
+
+@client.event
+async def on_message(message):
+    print('message')
+
+client.run(BOT_TOKEN)
