fbd3748652b899a34fc67d63fa79963d04c48076
Polyanin Mike <ya.polyanin-mike@yandex.ru>
2019-05-06 18:00:32 +0300

    Added core functionality

--- discord_inviter.py
+++ discord_inviter.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 from configparser import ConfigParser
 import asyncio
-from utils import save_dealer, get_member, off_dealer
+from utils import save_dealer, get_member, off_dealer, dealer_check
+from utils import parse_message_args
 
 config_file = 'set.ini'
 config = ConfigParser()
@@ -11,6 +12,11 @@ BOT_TOKEN = config['Bot']['token']
 
 client = commands.Bot(command_prefix = '.')
 
+INVITE_LIST = {}
+MAX_INVITES = 20
+MAX_TIMOUT = 300
+MIN_TIMOUT = 10
+
 @client.event
 async def on_ready():
     print('Ready')
@@ -18,7 +24,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print('message')
+    # print('message')
+    if message.channel.id in INVITE_LIST and not message.author.bot:
+        callback_channel = INVITE_LIST[message.channel.id].get('callback', None)
+        if callback_channel:
+            await callback_channel.send(f'{message.author.mention} says: {message.content}')
+        await message.channel.send('I am done')
+        INVITE_LIST.pop(message.channel.id)
     await client.process_commands(message)
 
 @client.event
@@ -32,22 +44,6 @@ async def on_member_remove(member):
 @client.command()
 async def ping(ctx):
     await ctx.send(f'pong! {round(client.latency * 1000)}ms')
-
-@client.command()
-async def invite(ctx):
-    print(ctx.message)
-    author = ctx.author
-    if not author.dm_channel:
-        print('there is no dm channel with this folk, creating one...')
-        await author.create_dm()
-        print('done')
-    p_channel = author.dm_channel
-    print('pm_channel: ', p_channel)
-    try:
-        await p_channel.send('hello')
-        await ctx.send(f'{author.mention} check direct')
-    except discord.errors.Forbidden:
-        await ctx.send(f'{author.mention} have blocked me!')
 
 
 @client.command(name='clear')
@@ -66,24 +62,86 @@ async def clear_bot_messages(ctx):
 
 @client.command(name = 'dealer')
 async def set_dealer(ctx, *targets):
-    print('dealer comand')
-    channel = ctx.channel
-    for slug in targets:
-        member = get_member(channel, slug)
-        if member:
-            save_dealer(channel, member, config_file)
-            await ctx.send(f'member {member.mention} now has some abilities')
+    sender = ctx.message.author
+    if await client.is_owner(sender):
+        print('dealer add command')
+        channel = ctx.channel
+        for slug in targets:
+            member = get_member(channel, slug)
+            if member:
+                save_dealer(channel, member, config_file)
+                await ctx.send(f'member {member.mention} now has some abilities')
 
 
 @client.command(name = 'rm_dealer')
 async def remove_dealer(ctx, *targets):
-    print('dealer comand')
+    sender = ctx.message.author
+    if await client.is_owner(sender):
+        print('dealer off command')
+        channel = ctx.channel
+        for slug in targets:
+            member = get_member(channel, slug)
+            if member:
+                off_dealer(channel, member, config_file)
+                await ctx.send(f'member {member.mention} removed from dealers list')
+
+
+@client.command(name='invite')
+async def invite_member(ctx, *args):
     channel = ctx.channel
-    for slug in targets:
-        member = get_member(channel, slug)
-        if member:
-            off_dealer(channel, member, config_file)
-            await ctx.send(f'member {member.mention} removed from dealers list')
+    sender = ctx.message.author
+    members, pars = parse_message_args(*args)
+
+    if pars[0] > MAX_INVITES:
+        pars[0] = MAX_INVITES
+    if pars[1] > MAX_TIMOUT:
+        pars[1] = MAX_TIMOUT
+    if pars[1] < MIN_TIMOUT:
+        pars[1] = MIN_TIMOUT
+
+    print('members: ', members)
+    print('pars: ', pars)
+
+    if dealer_check(channel, sender, config_file):
+
+        for mem_slug in members:
+            target = get_member(channel, mem_slug)
+
+            if not target.dm_channel:
+                # print('there is no dm channel with this folk, creating one...')
+                await target.create_dm()
+
+            p_channel = target.dm_channel
+
+            try:
+                # await p_channel.send('hello')
+                # await ctx.send(f'{target.mention} has been invited')
+                await inviter(p_channel, *pars, callback=ctx.channel)
+
+            except discord.errors.Forbidden:
+                await ctx.send(f'{target.mention} have blocked me!')
+        # await ctx.send('OK')targets
+
+async def inviter(p_channel, count = 10, timeout=30, callback=None):
+
+    INVITE_LIST[p_channel.id] = {
+        'count': count,
+        'callback': callback
+    }
+
+    while INVITE_LIST.get(p_channel.id, None):
+        counter = INVITE_LIST[p_channel.id]['count']
+        await p_channel.send('hello number {}, timeout = {} sec'.format(counter, timeout))
+        try:
+            if counter <= 1:
+                INVITE_LIST.pop(p_channel.id)
+                await p_channel.send('I am done')
+                return None
+            INVITE_LIST[p_channel.id]['count'] -= 1
+        except KeyError:
+            return None
+        await asyncio.sleep(timeout)
+
 
 
 client.run(BOT_TOKEN)
--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
 from configparser import ConfigParser
-import asyncio
+
+
+def dealer_check(channel, member, config_file):
+    ch_id = str(channel.id)
+    mem_id = str(member.id)
+    config = ConfigParser()
+    config.read(config_file)
+
+    if config.getboolean(ch_id, mem_id):
+        return True
+    return False
+
 
 
 def get_member(channel, slug:str):
@@ -36,3 +47,28 @@ def off_dealer(channel, member, conf_file):
             config[ch_id][mem_id] = 'False'
     with open(conf_file, 'w') as f:
         config.write(f)
+
+
+def parse_message_args(*args):
+    members = []
+    pars = []
+    for arg in args:
+        # print(arg)
+        # print(type(arg))
+        if arg.startswith('$'):
+            pars.append(int(arg[1:]))
+        else:
+            members.append(arg)
+    return members, pars
+
+
+# @dealer
+def check_func(a, b):
+    print(a)
+    print(b)
+    return 'hello'
+
+if __name__ == '__main__':
+    # check_func('a', 'b')
+    res = parse_message_args('1111', '0111', '2432', '$333', '$228')
+    print(res)
